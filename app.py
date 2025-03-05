cad497a3e9db4c7310a5d47c7d18ae7eb55c3d0b
appkalorder <appkalorder@gmail.com>
2025-03-05 00:33:12 -0500

    fix querys 3 & 4

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def query_to_dataframe(query):
     conn.close()
     return df
 
-def moto_mas_vendida():
+def moto_mas_vendida(): #check
     query = """
     SELECT m.brand || ' ' || m.model AS moto, COUNT(s.motorcycle_id) AS total_vendidos
     FROM sales s
@@ -33,7 +33,7 @@ def moto_mas_vendida():
     plt.xticks(rotation=45, ha='right')
     plt.show()
 
-def mes_mas_vendido():
+def mes_mas_vendido(): #check
     query = """
     SELECT TO_CHAR(sale_date, 'Month') AS mes, COUNT(*) AS total_ventas
     FROM sales
@@ -48,28 +48,30 @@ def mes_mas_vendido():
     plt.xticks(rotation=45)
     plt.show()
 
-def dia_mas_vendido():
+def dia_mas_vendido(): #check
     query = """
-    SELECT sale_date, COUNT(*) AS total_ventas
+    SELECT TO_CHAR(sale_date, 'YYYY-MM-DD') AS sale_date, COUNT(*) AS total_ventas
     FROM sales
     GROUP BY sale_date
     ORDER BY total_ventas DESC
     LIMIT 10
     """
     df = query_to_dataframe(query)
-    plt.plot(df['sale_date'], df['total_ventas'], marker='o', linestyle='-', color='red')
+    plt.figure(figsize=(10, 5))
+    plt.bar(df['sale_date'], df['total_ventas'], color='red')
     plt.xlabel('Fecha')
     plt.ylabel('Ventas')
     plt.title('Días con más ventas')
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=45, ha='right')
+    
     plt.show()
 
-def cliente_que_mas_compro():
+def cliente_que_mas_compro(): #check
     query = """
-    SELECT c.name, COUNT(s.customer_id) AS total_compras
+    SELECT c.first_name AS name, COUNT(s.customer_id) AS total_compras
     FROM sales s
-    JOIN customers c ON s.customer_id = c.customer_id
-    GROUP BY c.name
+    JOIN customers c ON s.customer_id = c.id
+    GROUP BY c.first_name
     ORDER BY total_compras DESC
     LIMIT 10
     """
